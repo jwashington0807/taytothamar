175015cdb4d2c64e1827c76b79eec6e777b5d245
JT Washington <jaytee.washington@gmail.com>
2023-05-15 10:36:54 -0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './Pages/portfolio/portfolio.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { ReferencesComponent } from './Pages/references/references.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortfolioComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'references', component: ReferencesComponent},
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes,{useHash:false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './Pages/portfolio/portfolio.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { ReferencesComponent } from './Pages/references/references.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent,
+    BannerComponent,
+    FooterComponent,
+    PortfolioComponent,
+    ContactComponent,
+    ReferencesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
