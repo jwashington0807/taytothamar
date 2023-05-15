1c457f09b293f79c683727939ee86bda0eb543fd
JT Washington <jaytee.washington@gmail.com>
2023-05-15 10:32:50 -0400

    initial?

---
+++ src/app/Pages/portfolio/portfolio.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from 'src/app/banner/banner.component';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent {
+
+  constructor() {
+    
+  }
+}
