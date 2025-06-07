76c7387e729767c9326d830421ec49f8d941f266
JT Washington <jaytee.washington@gmail.com>
2025-06-07 13:08:05 -0400

    updating into v19

--- src/app/Pages/references/references.component.ts
+++ src/app/Pages/references/references.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-references',
-  templateUrl: './references.component.html',
-  styleUrls: ['./references.component.scss']
+    selector: 'app-references',
+    templateUrl: './references.component.html',
+    styleUrls: ['./references.component.scss'],
+    standalone: false
 })
 export class ReferencesComponent {
 
